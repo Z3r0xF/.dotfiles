ba8c529941e6b59873d82860e1d784937dad98b3
Z3r0xF <Z3r0xF@proton.me>
2025-07-25 20:55:24 +0200

    Update

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 
 #Run this script after our Arch Linux installation is complete.
 #Install git first before running this script:
+sudo pacman -Sy
 sudo pacman -S --noconfirm git
 
 
@@ -24,13 +25,15 @@ fi
 # Read packages file and install them
 cat $PACKAGES_FILE | xargs sudo pacman -S --noconfirm
 
-#Install oh-my-zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-#Create symlinks with stow by running $HOME/.dotfiles/stow.sh from current directory
-./stow.sh
 
 
 #Enable services
 sudo systemctl enable opensnitchd
 sudo systemctl start opensnitchd
+
+
+#Install oh-my-zsh (Do this as last step to avoid conflicts because shell is set to zsh automatically)
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+#Create symlinks with stow by running $HOME/.dotfiles/stow.sh from current directory
+./stow.sh
